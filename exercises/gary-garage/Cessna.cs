6e78e4c267f61d1e7af7f7ada183cc46b4554bc9
joshuastewart417 <joshua.stewart780@gmail.com>
2021-12-01 11:52:55 -0600

    created interface.cs and implemented into interfaces into vehicle modules

--- exercises/gary-garage/Cessna.cs
+++ exercises/gary-garage/Cessna.cs
@@ -2,14 +2,16 @@ using System;
 
 namespace Garage
 {
-    public class Cessna : Vehicle
+    public class Cessna : Vehicle, IGasVehicle
     {
         public double FuelCapacity { get; set; }
 
         public void RefuelTank()
         {
-            // method definition omitted
+            CurrentTankPercentage = 100;
         }
+
+        public int CurrentTankPercentage { get; set; } = 73;
 
         public override void Drive()
         {
--- exercises/gary-garage/Ram.cs
+++ exercises/gary-garage/Ram.cs
@@ -2,16 +2,16 @@ using System;
 
 namespace Garage
 {
-    public class Ram : Vehicle
+    public class Ram : Vehicle, IGasVehicle
     {
         public double FuelCapacity { get; set; }
        
-
         public void RefuelTank()
         {
-            // method definition omitted
+            CurrentTankPercentage = 100;
         }
 
+        public int CurrentTankPercentage { get; set; } = 12;
         public override void Drive()
         {
             Console.WriteLine("The Black Ram zooms by you! AGHHHHHHH!!!!");
